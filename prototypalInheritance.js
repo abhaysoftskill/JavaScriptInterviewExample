1b86657277e59e6be42404c1b19a42bd515abd1a
Abhay Narnaware <70929237+abhaysoftskill@users.noreply.github.com>
2020-11-02 08:04:29 +0530

    Prototypal Inheritance

--- prototypalInheritance.js
+++ prototypalInheritance.js
@@ -11,3 +11,25 @@ console.log(hyundai.getModel())
 
 let tata = new car('tata');
 console.log(tata.getModel())
+
+
+// ***********************************
+let arr = ["Abhay", "kanchan"];
+let object = {
+    name: "Advik",
+    city:"Pune",
+    getIntro : function (){
+        console.log(this.name + "from " + this.city);
+    }
+}
+console.log(object.__proto__)
+function fun() {
+    
+}
+console.log(fun.__proto__.__proto__.__proto__)
+
+let object2 = {
+    name:"Son"
+}
+//never do this
+object2.__proto__ = object1;
